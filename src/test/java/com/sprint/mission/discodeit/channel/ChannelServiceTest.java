d07848a90357ef4870e3a36792a1df00fe7d7491
rorm0819 <rorm0819@gmail.com>
2026-03-31 10:04:39 +0900

    Sprint7

--- src/test/java/com/sprint/mission/discodeit/channel/ChannelServiceTest.java
+++ src/test/java/com/sprint/mission/discodeit/channel/ChannelServiceTest.java
@@ -1,5 +1,9 @@
 package com.sprint.mission.discodeit.channel;
 
+import com.sprint.mission.discodeit.dto.request.PrivateChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.request.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
@@ -8,6 +12,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class ChannelServiceTest {
@@ -20,46 +30,83 @@ class ChannelServiceTest {
     @Test
     void createPublicChannelPassTest() {
         // given
+        Channel channel = new Channel(
+                ChannelType.PUBLIC,
+                "공용 채널 통과",
+                "테스트 통과한 공용 채널입니다."
+        );
+        PublicChannelCreateRequest request = new PublicChannelCreateRequest(
+                channel.getName(),
+                channel.getDescription()
+        );
 
         // when
+        channelService.create(request);
 
         // then
+        verify(channelService, times(1))
+                .create(request);
     }
 
     @Test
     void createPublicChannelFailTest() {
         // given
+        PublicChannelCreateRequest request = new PublicChannelCreateRequest(
+                "공용 채널 생성 실패",
+                "생성 실패한 공용 채널입니다."
+        );
 
         // when
+        channelService.create(request);
 
         // then
+        verify(channelService, times(1))
+                .create(request);
     }
 
     @Test
     void createPrivateChannelPassTest() {
         // given
+        PrivateChannelCreateRequest request = new PrivateChannelCreateRequest(
+                new ArrayList<UUID>()
+        );
 
         // when
+        channelService.create(request);
 
         // then
+        verify(channelService, times(1))
+                .create(request);
     }
 
     @Test
     void createPrivateChannelFailTest() {
         // given
+        PrivateChannelCreateRequest request = new PrivateChannelCreateRequest(
+                new ArrayList<UUID>()
+        );
 
         // when
+        channelService.create(request);
 
         // then
+        verify(channelService, times(1))
+                .create(request);
     }
 
     @Test
     void updateChannelPassTest() {
         // given
+        PrivateChannelCreateRequest request = new PrivateChannelCreateRequest(
+                new ArrayList<UUID>()
+        );
 
         // when
+        channelService.create(request);
 
         // then
+        verify(channelService, times(1))
+                .create(request);
     }
 
     @Test
